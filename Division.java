946a8ec10939568f17f76f2b25fc30a7d967dc48
parth bhutada <parthbhutada13@gmail.com>
2025-04-19 14:47:47 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Division implements Operation {
+    @Override
+    public double calculate(double num1, double num2) throws ArithmeticException {
+        if (num2 == 0) {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
+        return num1 / num2;
+    }
+}
