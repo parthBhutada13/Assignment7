3632f6a484c1eb3157f4978191fadb93b19cffb8
parth bhutada <parthbhutada13@gmail.com>
2025-04-19 14:42:30 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+// Name: Parth Bhutada
+// PRN: 23070126084
+// Batch: AIML B1
+
+package calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+}
